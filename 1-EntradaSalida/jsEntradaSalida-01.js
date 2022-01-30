b2ea26dcb80381c179ff55f3a722c042c132f39e
unknown <nicolasg.alonso@gmail.com>
2022-01-30 17:23:39 -0300

    estado normal

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Todo bien, todo correcto? Y yo que me alegro");
+	alert("ok");
 }
 
